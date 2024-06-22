ccd91cfeec94fb6b2cba09f3496dfbddf6454540
Aditi kumari <ojhaaditihehe@gmail.com>
2024-06-22 03:02:28 +0000

    largest element in an array

---
+++ Array/Easy_level/Largest_element.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <climits> // For INT_MIN
+using namespace std;
+
+/* 
+Brute force approach: Traverse the array and keep track of the maximum value found so far.
+*/
+
+int maxvalue(vector<int>& nums) {
+    int size = nums.size();
+    // Step 1: Take a max variable and assign its value to INT_MIN
+    int maxi = INT_MIN;
+    // Step 2: Run a loop from i=0 to i<size, and every time we come across a value larger than our max, update it 
+    for (int i = 0; i < size; i++) {
+        if (nums[i] > maxi) {
+            maxi = nums[i];
+        }
+    }
+    return maxi;
+}
+
+// Time complexity -> O(N)
+int main() {
+    cout << "Enter the size of the array: ";
+    int s;
+    cin >> s;
+    vector<int> nums(s);
+    cout << "Enter the elements of the array:\n";
+    for (int i = 0; i < s; i++) {
+        cin >> nums[i];
+    }
+    int largest = maxvalue(nums);
+    cout << "The largest value in the array is: " << largest << endl;
+    return 0;
+}
